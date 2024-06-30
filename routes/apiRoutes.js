314521b1e4cc6fc0b11c92ec6966f230a83a2aad
Elena Papanikolas <epapanikolas@hotmail.com>
2024-06-30 16:14:47 -0600

    Created delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -45,5 +45,35 @@ api.post('/notes', (req, res) => {
     });
 });
 
+// Delete request to delete notes
+api.delete('/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            const notes = JSON.parse(data);
+            const { id } = req.params;
+            const index = notes.findIndex(note => note.id ===id);
+
+            if (index !== -1) {
+                notes.splice(index, 1);
+                fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+                    if (err) {
+                        console.error(err);
+                        return res.status(500).json({ error: 'Internal Server Error' });
+                    }
+                });
+
+                    res.status(204).json(notes);
+                } else {
+                    res.status(404).json({error: 'No note found'});
+                }
+        } 
+    });
+    
+});
+
+
 // Exporting
 module.exports = api;
