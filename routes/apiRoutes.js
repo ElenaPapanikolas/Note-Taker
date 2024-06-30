267405ec59ceb5399900d6c175e8e6162e9612b1
Elena Papanikolas <epapanikolas@hotmail.com>
2024-06-30 15:34:22 -0600

    Generated random uuids for eac note and assigned to notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+// npm package to generate unique ids for each note
+const { v4: uuidv4 } = require('uuid');
 const api = express();
 
-api.use(express.json()); // Middleware to parse JSON body
-
 // Define a route to read the db.json file and return all saved notes as JSON
-api.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+api.get('/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ error: 'Internal Server Error' });
@@ -20,11 +19,13 @@ api.get('/api/notes', (req, res) => {
 });
 
 // Define a route to receive a new note, add it to db.json, and return the new note
-api.post('/api/notes', (req, res) => {
+api.post('/notes', (req, res) => {
     const newNote = req.body;
-    /////////////// // Generate a unique ID for the new note
+    // Generate a unique ID for the new note
+    newNote.id = uuidv4();
+    
 
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ error: 'Internal Server Error' });
@@ -33,7 +34,7 @@ api.post('/api/notes', (req, res) => {
         const notes = JSON.parse(data);
         notes.push(newNote);
 
-        fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2), (err) => {
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (err) => {
             if (err) {
                 console.error(err);
                 return res.status(500).json({ error: 'Internal Server Error' });
